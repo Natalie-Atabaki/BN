1525d01252798c9259f056e7d0b7ffc4f7da1c23
Natalie-Atabaki <natalie.atabaki@sund.ku.dk>
2025-06-02 15:31:42 +0200

    Update 02_Download_pQTLPanel.py

--- scripts/Daniel/02_Download_pQTLPanel.py
+++ scripts/Daniel/02_Download_pQTLPanel.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Login
 syn = synapseclient.Synapse()
-PROJFOLDER = '/Users/da1078co/Documents/Lund/PhD/Projects/BN_Naeimeh/'
+PROJFOLDER = '~/'
 TOKEN = open(PROJFOLDER + 'data/synapse_tokenfile.txt','r').read()
 syn.login(authToken=TOKEN)
 
